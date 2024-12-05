fea87b50f80d9ce7d6d4980b5bea820e46a7c123
kolimate <115666713+kolimate@users.noreply.github.com>
2024-12-05 19:15:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import getpass
 import os
 from langchain_groq import ChatGroq
 from docx import Document
-
+from chainlit.input_widget import Slider
 
 if "GROQ_API_KEY" not in os.environ:
     #os.environ["GROQ_API_KEY"] = getpass.getpass("Enter your Groq API key: ")
